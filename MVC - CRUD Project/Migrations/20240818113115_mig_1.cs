0f086baa57c3f1482cbf04a8009a7d10f8421f18
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-18 15:34:39 +0400

    Migrations were Created

---
+++ MVC - CRUD Project/Migrations/20240818113115_mig_1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC___CRUD_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mens",
+                columns: table => new
+                {
+                    Men_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Men_FullName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mens", x => x.Men_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mens");
+        }
+    }
+}
