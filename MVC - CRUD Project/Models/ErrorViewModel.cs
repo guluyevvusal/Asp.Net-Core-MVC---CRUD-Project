1fe28096cd2da3ac0dc8572301951dc7992734f1
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-18 14:54:14 +0400

    Add project files.

---
+++ MVC - CRUD Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC___CRUD_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
